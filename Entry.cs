1f69e217ff49e968ac9ec7e5062ad122dd16e623
JonathanQ-Maker <jonathanqfxw@gmail.com>
2025-07-26 07:26:58 -0700

    models update

--- Entry.cs
+++ Entry.cs
@@ -2,16 +2,8 @@ using UnityEngine;
 
 public class Entry : MonoBehaviour
 {
-    public TextureAnimator animator;
-    public int frame;
     void Start()
     {
         Application.targetFrameRate = 100;
-    }
-
-
-    private void Update()
-    {
-        animator.Frame = frame;
     }
 }
--- Usable/VendingMachine.cs
+++ Usable/VendingMachine.cs
@@ -4,8 +4,11 @@ public class VendingMachine : MonoBehaviour, IUsable
 {
     public Vector3 Position => transform.position;
 
+    [SerializeField]
+    private Animator animator;
+
     public void Use()
     {
-        
+        animator.SetTrigger("HatchFlip");
     }
 }
